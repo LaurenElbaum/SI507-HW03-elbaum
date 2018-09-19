25dc45ac16cc331d3383c720d02b840de244392b
Kyle <kylegomi@gmail.com>
2018-09-19 11:35:10 -0400

    Added Kyle's Questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,6 +1,22 @@
+
+import random
+
 def ask_group():
 	n = input("What is your question?")
 	return n
 
 x = ask_group()
 #print(x)
+
+answers = [
+           'Reply hazy, try again',
+           'Ask again later.',
+           'Better not tell you now.',
+           'Cannot predict now.',
+           'Concentrate and ask again.',
+           'Do not count on it.',
+           'My reply is no.',
+           'My sources say no.',
+           'Outlook not so good.',
+           'Very doubtful.']
+print(random.choice(answers))
