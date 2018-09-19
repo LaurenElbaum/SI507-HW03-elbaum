79515c88fb3294bf4ea1476ce2e6ffe74bbfa51e
Lauren Elbaum <lauren.elbaum@gmail.com>
2018-09-19 11:51:19 -0400

    third commit

--- magic_eight.py
+++ magic_eight.py
@@ -1,6 +1,23 @@
 def ask_group():
 	n = input("What is your question?")
-	return n
+	while n != "quit":
+
+		if n.endswith('?'):
+			print("Great!") 
+		else:    
+			print("I’m sorry, I can only answer questions.")
+
+		n = input("What is your question?")	
+
+	
 
 x = ask_group()
 #print(x)
+
+
+"""
+if input("What is your question?").endswith('?'):
+    print("Great!") 
+else:    
+    print("I’m sorry, I can only answer questions.")
+"""
